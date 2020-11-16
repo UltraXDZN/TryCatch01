4280c8bc7a156ac8d735bcbf8883ba397cfaed15
UltraXDZN <57915175+UltraXDZN@users.noreply.github.com>
2020-11-16 09:01:19 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatch01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+            Console.Write("Unesi broj: ");
+            try
+            {
+                broj = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Broj je: {broj}.");
+            }
+            catch (Exception greska)
+            {
+                Console.WriteLine(greska.Message);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
